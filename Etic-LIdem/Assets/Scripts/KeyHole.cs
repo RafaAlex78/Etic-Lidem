7a0b0ca79cfd4d5a98acd9e152ce88cacdd75301
MIguel Lampreia <miguel_lampreia@eticalgarve.com>
2022-11-28 12:35:57 +0000

    Final Puzzle 3 keys working Keypad Activates Keypad in progress

--- Etic-LIdem/Assets/Scripts/KeyHole.cs
+++ Etic-LIdem/Assets/Scripts/KeyHole.cs
@@ -8,15 +8,29 @@ public class KeyHole : MonoBehaviour
     [SerializeField] private GameObject handle;
     [SerializeField] private GameObject key;
     [SerializeField] private GameObject block;
+    [SerializeField] private FinalPuzzle final;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Key")
         {
             other.gameObject.SetActive(false);
-            key.SetActive(true);
-            handle.SetActive(true);
-            block.SetActive(false);
+            if (key != null)
+            {
+                key.SetActive(true);
+            }
+            if (handle != null)
+            {
+                handle.SetActive(true);
+            }
+            if (block != null)
+            {
+                block.SetActive(false);
+            }
+            if (final != null)
+            {
+                final.InsertKey();
+            }
         }
     }
 }
