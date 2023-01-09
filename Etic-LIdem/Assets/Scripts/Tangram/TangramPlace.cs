706fbeb700652e7670235022e829672a52c069a1
RafaAlex78 <102955489+RafaAlex78@users.noreply.github.com>
2023-01-09 16:09:47 +0000

    Tangram Puzzle Code

---
+++ Etic-LIdem/Assets/Scripts/Tangram/TangramPlace.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangramPlace : MonoBehaviour
+{
+    [SerializeField] GameObject _rightTangramPiece;
+
+    public GameObject RightTangramPiece { get => _rightTangramPiece; set => _rightTangramPiece = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
