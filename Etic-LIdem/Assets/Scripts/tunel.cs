cef308bf2814b3d154c4d6ea261cfc2c43d3faf7
RAFA\Rafael <rafae>
2023-02-23 17:37:05 +0000

    Fixed the tunnel scale

--- Etic-LIdem/Assets/Scripts/tunel.cs
+++ Etic-LIdem/Assets/Scripts/tunel.cs
@@ -13,7 +13,26 @@ public class tunel : MonoBehaviour
 
     [SerializeField] bool _onTheBigRoom;
 
+    private void Update()
+    {
+        RoomUpdate();
+    }
+    void RoomUpdate()
+    {
+        if (_onTheBigRoom)
+        {
+            _mapToScale.transform.localScale = _smallRoomScale;
+            _mapToScale.transform.position = _smallRoomPos;
+           // _onTheBigRoom = false;
 
+        }
+        else
+        {
+            _mapToScale.transform.localScale = _bigRoomScale;
+            _mapToScale.transform.position = _bigRoomPos;
+            //_onTheBigRoom = true;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
